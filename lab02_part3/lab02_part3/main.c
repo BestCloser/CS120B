f43692250fc56b01d08b96f7c2094e3c751952e2
Daniel <dli015@ucr.edu>
2018-08-01 00:59:05 -0700

    finished non-challenges for lab 2
    fixed part 2 for consistency
    finished part 3

--- lab02_part3/lab02_part3/main.c
+++ lab02_part3/lab02_part3/main.c
@@ -1,5 +1,5 @@
 /*
- * lab02_part2.c
+ * lab02_part3.c
  *
  * Created: 7/31/2018 3:17:30 PM
  * Author : Daniel
@@ -22,12 +22,13 @@ int main(void)
     DDRA = 0x00; PORTA = 0xFF; //PA3 ... PA0 value between 0 and 15 (full)
 	DDRB = 0xFF; PORTC = 0x00; //PC5 ... PC0 LED for fuel level, lv1 or 2 = PC5; lv3 or 4, PC5 and PC4; lv5 or 6, PC5,4,3 ; etc ; PC6 is LOW FUEL, lights up if level is 4 or less
     unsigned char tmpA = 0x00;
+	unsigned char tmpA_2 = 0x00;
 	unsigned char tmpC = 0x00;
 
-	while(1)
-	{
-		tmpA = PINA & 0x0F; //bitmask, only want PA3,2,1,0
-		tmpC = 0x00;
+	while(1) 
+    {
+		tmpA = PINA & 0x0F; //bitmask for PA3,2,1,0 ; PA4 is now [if key is in the ignition], PA5 is [if driver is seated], PA6 is [if seatbelt is fastened]
+		tmpC = 0x00;	
 		if (tmpA == 0) { //PC6 on, nothing else
 			tmpC = SetBit(tmpC, 6, 1);
 		}
@@ -70,8 +71,12 @@ int main(void)
 			tmpC = SetBit(tmpC, 1, 1);
 			tmpC = SetBit(tmpC, 0, 1);
 		}
+		
+		tmpA_2 = PINA & 0x70; //bitmask for PA6,5,4
+		if ( GetBit(tmpA_2, 4) && GetBit(tmpA_2, 5) && !GetBit(tmpA_2, 6) ) {
+			tmpC = SetBit(tmpC, 7, 1);
+		}
 
-
-		PORTC = tmpC & 0x7F;
-	}
+		PORTC = tmpC;
+    }
 }
