74101f5684539920d17f4253ec1e14165e676a7a
Daniel <dli015@ucr.edu>
2018-08-02 00:59:22 -0700

    fixed lab03 part1
    added waits to stabilize

--- lab03_part1/lab03_part1/main.c
+++ lab03_part1/lab03_part1/main.c
@@ -1,4 +1,4 @@
-/*
+	/*
  * lab03_part1.c
  *
  * Created: 8/1/2018 1:51:51 PM
@@ -8,7 +8,7 @@
 #include <avr/io.h>
 //#include "RIMS.h"
 
-enum LED_states { INIT, PB0_ON, PB1_ON } LED_State;
+enum LED_states { INIT, WAIT_01, PB1_ON, WAIT_10, WAIT_11, PB0_ON, WAIT_00 } LED_State;
 
 void LED_Tick()
 {
@@ -16,12 +16,13 @@ void LED_Tick()
 
 	switch(LED_State) {   // Transitions
 		case INIT:  // Initial transition
-		LED_State = PB0_ON;
+		PORTB = 0x01; //init condition
+		LED_State = WAIT_01;
 		break;
 
-		case PB0_ON:
+		case WAIT_01:
 		if (!tmpA) {
-			LED_State = PB0_ON;
+			LED_State = WAIT_01;
 		}
 		else if (tmpA) {
 			LED_State = PB1_ON;
@@ -29,11 +30,39 @@ void LED_Tick()
 		break;
 
 		case PB1_ON:
+
+		LED_State = WAIT_10;
+
+		break;
+
+		case WAIT_10:
 		if (!tmpA) {
-			LED_State = PB1_ON;
+			LED_State = WAIT_11;
+		}
+		else if (tmpA) {
+			LED_State = WAIT_10;
+		}
+		break;
+
+		case WAIT_11:
+		if (!tmpA) {
+			LED_State = WAIT_11;
 		}
 		else if (tmpA) {
 			LED_State = PB0_ON;
+		}
+		break;
+
+		case PB0_ON:
+		LED_State = WAIT_00;
+		break;
+
+		case WAIT_00:
+		if (!tmpA) {
+			LED_State = WAIT_01;
+		}
+		else if (tmpA) {
+			LED_State = WAIT_00;
 		}
 		break;
 
@@ -43,6 +72,11 @@ void LED_Tick()
 	} // Transitions
 
 	switch(LED_State) {   // State actions
+
+		case INIT:
+		PORTB = 0x01; //init PB0 = 1; PB1 = 0;
+		break;
+		
 		case PB0_ON:
 		//PB0 = 1, PB1 = 0
 		PORTB = 0x01;
