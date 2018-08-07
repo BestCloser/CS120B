ae4214aa29cc7811ab8b9c5b7240fc06f84812dd
Daniel <dli015@ucr.edu>
2018-08-07 12:46:14 -0700

    lol just a prank NOW it is done
    pranked myself lol

--- lab04_part2/lab04_part2/main.c
+++ lab04_part2/lab04_part2/main.c
@@ -1,107 +1,129 @@
 /*
- * lab3p2.c
+ * lab03_part2.c
  *
- * Created: 8/1/2018 1:28:50 PM
- * Author : ucrcse
- */ 
+ * Created: 8/1/2018 2:43:26 PM
+ * Author : Daniel
+ */
 
-#include <avr/io.h> 
+#include <avr/io.h>
+//#include "RIMS.h"
 
-enum c_val{init, wait0, dec_wait, decC, incC, inc_wait,clr_c}operateC;
-	
-	void changeC(){
-	  unsigned char pa0 = PINA & 0x01;
-	  unsigned char pa1 = PINA & 0x02;
- 		 switch(operateC){
-			case init:
-				PORTC = 7;
-				operateC = wait0;
-			break;
-			
-			case wait0:
-				if (~PINA == 3)   //if both on
-				{
-					operateC = clr_c;
-				}
-				else if(~PINA == 1) //inc_state
-				{
-					operateC = incC;
-				}
-				else if (~PINA == 2) //dec_state
-				{
-					operateC = decC;
-				}
-				else{
-					//do nothing
-				}
-			break;
-			
-			case dec_wait:
-				if (~PINA == 0)
-				{
-					operateC = wait0;
-				}
-				else if (~PINA == 3)
-				{
-					operateC = clr_c;
-				}
-				else{
-					//whatever
-				}
-			break;
-			
-			case decC:
-				if (PORTC > 0)
-				{
-					PORTC--;
-				}
-				operateC = dec_wait;
-			break;
-				
-			case incC:
-				if (PORTC < 9)
-				{
-					PORTC++;
-				}
-				operateC = dec_wait;
-			break;
-			
-			case inc_wait:
-				if (~PINA == 0)
-				{
-					operateC = wait0;
-				}
-				else if (~PINA == 3)
-				{
-					operateC = clr_c;
-				}
-				else{
-					//whatever
-				}
-			break;
-			
-			case clr_c:
-			PORTC = 0;
-			if (~PINA != 3)
-			{
-				operateC = wait0;
-			}
-			
-			break;
-			
-			default:
-			operateC = wait0;
-			break;
-			}
-		return;
-		};
-int main(void)
+enum PORTC_states { INIT, MAIN_WAIT, ADD, ADD_WAIT, SUBTRACT, SUB_WAIT, RESET, RESET_WAIT } PORTC_State;
+
+void PORTC_Tick()
 {
-    /* Replace with your application code */
-	DDRA = 0x00; PORTA = 0xFF; 
-	DDRC = 0xFF; PORTC = 0x00; 
-    while (1) 
-    {
-		changeC();
-    }
+	unsigned char A0 = ~PINA & 0x01; //bitmasks, want PA0 and PA1	
+	unsigned char A1 = ~PINA & 0x02;
+	unsigned char tmpC = PORTC & 0x0F; //max 9, min 0
+	switch(PORTC_State) {   // Transitions
+		case INIT:  // Initial transition
+		PORTC = 0x07;
+		PORTC_State = MAIN_WAIT;
+		break;
+		
+		case MAIN_WAIT:
+		if ( !A0 && !A1 ) {
+			PORTC_State = MAIN_WAIT;
+		}
+		else if ( A0 && !A1 ) {
+			PORTC_State = ADD;
+		}
+		else if ( !A0 && A1 ) {
+			PORTC_State = SUBTRACT;
+		}
+		else if ( A0 && A1 ) {
+			PORTC_State = RESET;
+		}
+		break;
+
+		case ADD:
+		PORTC_State = ADD_WAIT;
+		break;
+
+		case ADD_WAIT:
+		if ( !A0 ) { //both (!A0 && !A1) and (!A0 && A1)
+			PORTC_State = MAIN_WAIT;
+		}
+		else if ( A0 && !A1 ) {
+			PORTC_State = ADD_WAIT;
+		}
+		else if ( A0 && A1 ) {
+			PORTC_State = RESET;
+		}
+		break;
+
+		case SUBTRACT:
+		PORTC_State = SUB_WAIT;
+		break;
+
+		case SUB_WAIT:
+		if ( !A1 ) { //includes (!A0 && !A1) and (A0 && !A1)
+			PORTC_State = MAIN_WAIT;
+		}
+		else if ( !A0 && A1 ) {
+			PORTC_State = SUB_WAIT;
+		}
+		else if ( A0 && A1 ) {
+			PORTC_State = RESET;
+		}
+		break;
+		
+		case RESET:
+		PORTC_State = RESET_WAIT;
+		break;
+
+		case RESET_WAIT:
+		if ( !A0 && !A1 ) {
+			PORTC_State = MAIN_WAIT;
+		}
+		else {
+			PORTC_State = RESET_WAIT;
+		}
+		break;
+		
+		default:
+		PORTC_State = INIT;
+		break;
+	} // Transitions
+
+	switch(PORTC_State) {   // State actions
+		case INIT:
+		tmpC = 0x07;
+		PORTC = tmpC; //init = 7
+		break;
+
+		case ADD:
+		if (tmpC < 9) {
+			tmpC = tmpC + 1;
+		} //else tmpC is already 9
+		PORTC = tmpC;
+		break;
+
+		case SUBTRACT:
+		if (tmpC > 0) {
+			tmpC = tmpC - 1;
+		} //else tmpC is already 0
+		PORTC = tmpC;
+		break;
+
+		case RESET:
+		//tmpC = 0x00;
+		PORTC = 0x00;
+		break;
+
+		default:
+		break;
+	} // State actions
 }
+
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;              // Initialize outputs
+	PORTC_State = INIT; // Indicates initial call
+
+	while(1) {
+		PORTC_Tick();
+	}
+}
+
