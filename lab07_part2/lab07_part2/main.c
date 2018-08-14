cccbf381facca2420bf94a1d84f0ea8626016ff4
Daniel <dli015@ucr.edu>
2018-08-14 12:12:33 -0700

    fixed lab7p2

--- lab07_part2/lab07_part2/main.c
+++ lab07_part2/lab07_part2/main.c
@@ -1,5 +1,5 @@
 /*
- * lab07_part1.c
+ * lab07_part2.c
  *
  * Cated: 8/13/2018 1:47:27 PM
  * Author : Daniel
@@ -9,42 +9,34 @@
 //#include "RIMS.h"
 #include "timer.c"
 
-enum ThreeLED_States { ThreeLED_Start, ONE, TWO, THREE } ThreeLED_State;
+enum ThreeLED_States { ONE, TWO, THREE } ThreeLED_State;
 	
 void ThreeLED_Tick() {
 	switch(ThreeLED_State) {
-		case ThreeLED_Start:
-		PORTB = 0x00;
-		ThreeLED_State = ONE;
-		break;
 		
 		case ONE:
-		PORTB = 0x01;
+		PORTB = (PORTB & 0x08) | 0x01;
 		ThreeLED_State = TWO;
 		break;
 		
 		case TWO:
-		PORTB = 0x02;
+		PORTB = (PORTB & 0x08) | 0x02;
 		ThreeLED_State = THREE;
 		break;
 		
 		case THREE:
-		PORTB = 0x04;
+		PORTB = (PORTB & 0x08) | 0x04;
 		ThreeLED_State = ONE;
 		break;
 	}
 }
 
-enum BlinkingLED_States { BlinkingLED_Start, OFF, ON } BlinkingLED_State;
+enum BlinkingLED_States { OFF, ON } BlinkingLED_State;
 void BlinkingLED_Tick() {
 	switch(BlinkingLED_State) {
-		case BlinkingLED_Start:
-		//PORTB = 0;
-		ThreeLED_State = OFF;
-		break;
 		
 		case OFF:
-		PORTB = PORTB | 0x00;
+		PORTB = PORTB & 0x07;
 		BlinkingLED_State = ON;
 		break;
 		
@@ -58,29 +50,27 @@ void BlinkingLED_Tick() {
 int main(void) {
 	DDRB = 0xFF; PORTB = 0x00;
 	 
-//	unsigned long ThreeLED_elapsedTime = 0;
-//	unsigned long BlinkingLED_elapsedTime = 0;
-	const unsigned long timerPeriod = 125;
+	unsigned long ThreeLED_elapsedTime = 0;
+	unsigned long BlinkingLED_elapsedTime = 0;
+	const unsigned long timerPeriod = 38;
 	//B = 0;
-	TimerSet(125);
+	TimerSet(38);
 	TimerOn();
 	ThreeLED_State = ONE;
 	BlinkingLED_State = OFF;
 	while (1) {
-//		if (ThreeLED_elapsedTime >= 125) {
+		if (ThreeLED_elapsedTime >= 38) { //125 is 1000ms, 300ms is 125*.3 = 37.5 => 38 
 			ThreeLED_Tick();
-//			ThreeLED_elapsedTime = 0;
-//		}
-		
-//		if (BlinkingLED_elapsedTime >= 125) {	
+			ThreeLED_elapsedTime = 0;
+		}
+		if (BlinkingLED_elapsedTime >= 125) {
 			BlinkingLED_Tick();
-//			BlinkingLED_elapsedTime = 0;
-//		}
-		
+			BlinkingLED_elapsedTime = 0;
+		}
 		while (!TimerFlag){} // Wait for timer period
 		TimerFlag = 0; // Lower flag raised by timer
 
-//		ThreeLED_elapsedTime += timerPeriod;
-//		BlinkingLED_elapsedTime += timerPeriod;
+		ThreeLED_elapsedTime += timerPeriod;
+		BlinkingLED_elapsedTime += timerPeriod;
 	}
 }
